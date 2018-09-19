850e0d952505bcf855d7df7dfe2d3c3516189903
Ravi Assis <32408043+raviassis@users.noreply.github.com>
2018-09-19 11:24:46 -0300

    Add files via upload

--- Semestre3/LabCompIII/quickSort/src/quickSort/Application.java
+++ Semestre3/LabCompIII/quickSort/src/quickSort/Application.java
@@ -1,82 +1,117 @@
-package quickSort;
 
 public class Application {
-	
-	private static int quantidade = 10000;
-	
-	
-	private static long[][] expQuickSort = new long [10][10];
-	private static long [][] expHeapSort = new long [10][10];
 
+	private static int quantidade = 10000;	
+
+	private static double[] expQuickSort = new double[10];
+	private static double[] expHeapSort = new double[10];
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        long start, end;
-		int [] vetor;  
-        
+		long start, end;
+		int [] vetor;        
+
 		for (int h = 1; h <= 10; h++) {
-			for (int i = 1; i <= 10; i++) {
-				
+			
+			long sQ = 0, sH = 0;
+			for (int i = 1; i <= 10; i++) {			
+
 				vetor = factoryRandomArray(quantidade * i);
 				start = System.currentTimeMillis();
 		        QuickSort.sort(vetor);
-		        end = System.currentTimeMillis();
+		        end = System.currentTimeMillis();		        
 		        
-		        expQuickSort[h-1][i-1] = end - start;
-		        
+		        sQ += end - start; 
 		        
 		        vetor = factoryRandomArray(quantidade * i);
 				start = System.currentTimeMillis();
 		        HeapSort.sort(vetor);
-		        end = System.currentTimeMillis();
+		        end = System.currentTimeMillis();		        
+		        sH += end -start;
 		        
-		        expHeapSort[h-1][i-1] = end - start;
+
 			}
-		}
+			
+			expQuickSort[h-1] = sQ / 10;
+			expHeapSort[h-1] = sH / 10;
+
+		}		
 		
-		double [] mediasQuick = new 
-		
+
 		System.out.println("Média do Quick Sort = " + media(expQuickSort));
-		System.out.println("Desvio padrão Quick Sort = " + desvioPadrao(expQuickSort));
-		
+
+		System.out.println("Desvio padrão Quick Sort = " + desvioPadrao(expQuickSort));		
+
 		System.out.println("\nMédia do Heap Sort = " + media(expHeapSort));
+
 		System.out.println("Desvio padrão Heap Sort = " + desvioPadrao(expHeapSort));
-        
-        
-        
+		
 	}
 	
 	private static int [] factoryRandomArray(int tam){
-		int [] vet = new int[tam];;
+
+		int [] vet = new int[tam];
+
 		
+
 		for (int i = 0; i < vet.length; i++) {
+
 			vet[i] = (int) ( Math.random() * tam);
+
             
+
 		}
+
 		
+
 		return vet;
+
 	}
+
 	
-	private static double media(long [] vet){
+
+	private static double media(double [] vet){
+
 		
-		double soma = 0, media;
+
+		double soma = 0;
+
 		
+
 		for (int i = 0; i < vet.length; i++) {
+
 			soma += vet[i]; 
+
 		}
+
 		
+
 		return soma / vet.length;
+
 	}
+
 	
-	private static double desvioPadrao(long [] vet){
+
+	private static double desvioPadrao(double [] vet){
+
 		double media = media(vet);
+
 		double desvio = 0;
+
 		int tam = vet.length;
+
 		
-		for (long vlr : vet) {
-	        Double aux = vlr - media;
+
+		for (double vlr : vet) {
+
+	        double aux = vlr - media;
+
 	        desvio += aux * aux;
+
 	    }
+
 	    return Math.sqrt(desvio / (tam - 1));
+
 	}
 
 }
