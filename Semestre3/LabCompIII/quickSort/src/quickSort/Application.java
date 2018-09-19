64568b19810d53623266badb38218ecba8313c78
Ravi Assis <32408043+raviassis@users.noreply.github.com>
2018-09-19 12:22:07 -0300

    Add files via upload

--- Semestre3/LabCompIII/quickSort/src/quickSort/Application.java
+++ Semestre3/LabCompIII/quickSort/src/quickSort/Application.java
@@ -1,7 +1,7 @@
 
 public class Application {
 
-	private static int quantidade = 10000;	
+	private static int quantidade = 100000;	
 
 	private static double[] expQuickSort = new double[10];
 	private static double[] expHeapSort = new double[10];
@@ -10,32 +10,23 @@ public class Application {
 		// TODO Auto-generated method stub
 		long start, end;
 		int [] vetor;        
+		for (int i = 1; i <= 10; i++) {			
 
-		for (int h = 1; h <= 10; h++) {
-			
-			long sQ = 0, sH = 0;
-			for (int i = 1; i <= 10; i++) {			
+			vetor = factoryRandomArray(quantidade);
+			start = System.currentTimeMillis();
+	        QuickSort.sort(vetor);
+	        end = System.currentTimeMillis();		        
+	        
+	        expQuickSort[i-1] = end - start;
+	        
+	        vetor = factoryRandomArray(quantidade);
+			start = System.currentTimeMillis();
+	        HeapSort.sort(vetor);
+	        end = System.currentTimeMillis();		        
+	 
+	        expHeapSort[i-1] = end -start;
 
-				vetor = factoryRandomArray(quantidade * i);
-				start = System.currentTimeMillis();
-		        QuickSort.sort(vetor);
-		        end = System.currentTimeMillis();		        
-		        
-		        sQ += end - start; 
-		        
-		        vetor = factoryRandomArray(quantidade * i);
-				start = System.currentTimeMillis();
-		        HeapSort.sort(vetor);
-		        end = System.currentTimeMillis();		        
-		        sH += end -start;
-		        
-
-			}
-			
-			expQuickSort[h-1] = sQ / 10;
-			expHeapSort[h-1] = sH / 10;
-
-		}		
+		}
 		
 
 		System.out.println("Média do Quick Sort = " + media(expQuickSort));
